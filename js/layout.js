6f89210a296fc49873e667cca7280bbc8fab8574
Victor Sodré <victor.sodres@gmail.com>
2015-10-23 00:27:45 -0200

    Mudancinhas
    Cor das portas
    Criação da classe carta
    Lista de assuntos importantes e de personagens

--- js/layout.js
+++ js/layout.js
@@ -1,18 +1,24 @@
 (function(){
-  function backColor(colorName) {
+
+  var backColor = function(colorName) {
     return { background: colorName };
   }
 
-  // Pintando as bordas
-  for(var i = 0; i < b.cols(); i++){
-    b.cell([0, i]).style(backColor('light'));
-    b.cell([b.rows()-1, i]).style(backColor('gray'));
-  }
+  var borderColor = backColor('gray');
+  var doorColor = 'rgb(255, 255, 139)';
 
-  for(var i = 0;i < b.rows(); i++){
-    b.cell([i, 0]).style(backColor('gray'));
-    b.cell([i, b.cols()-1]).style(backColor('gray'));
-  }
+  // Pintando as bordas
+  (function fillBorderCells(){
+    for(var i = 0; i < b.cols(); i++){
+      b.cell([0, i]).style(borderColor);
+      b.cell([b.rows()-1, i]).style(borderColor);
+    }
+
+    for(var i = 0;i < b.rows(); i++){
+      b.cell([i, 0]).style(borderColor);
+      b.cell([i, b.cols()-1]).style(borderColor);
+    }
+  })();
 
   // Pintando o lugar onde cada personagem inicia
   b.cell([5, 0]).style(backColor('purple'));
@@ -43,10 +49,37 @@
   //CENTRO - CLUE
   fillRoomColors(8, 15, 9, 5);
   //SALA - DINING ROOM
-  fillRoomColors(9, 15, 17, 7);
+  fillRoomColors(9, 15, 16, 7);
   //SALA - BALLROOM
   fillRoomColors(17, 23, 9, 6);
   fillRoomColors(23, 24, 10, 4);
   //SALA - KITCHEN
   fillRoomColors(17, 24, 17, 7);
+
+  /* Pintando as Portas */
+  var doorArray = [
+    b.cell([3, 5]).DOM(),
+    b.cell([4, 9]).DOM(),
+    b.cell([6, 11]).DOM(),
+    b.cell([6, 12]).DOM(),
+    b.cell([5, 18]).DOM(),
+    b.cell([9, 18]).DOM(),
+    b.cell([12, 16]).DOM(),
+    b.cell([8, 6]).DOM(),
+    b.cell([9, 3]).DOM(),
+    b.cell([11, 1]).DOM(),
+    b.cell([15, 5]).DOM(),
+    b.cell([19, 5]).DOM(),
+    b.cell([19, 9]).DOM(),
+    b.cell([19, 14]).DOM(),
+    b.cell([17, 10]).DOM(),
+    b.cell([17, 13]).DOM(),
+    b.cell([17, 18]).DOM()
+  ];
+
+  doorArray.forEach(function(doorElement){
+    doorElement.style.backgroundColor = doorColor;
+    doorElement.classList.remove('room-cell');
+  });
+
 })();
